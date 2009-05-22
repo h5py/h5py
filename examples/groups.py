fd4e9e77835a6148f9cbb62f4bd6f6309df59c13
andrewcollette <andrew.collette@gmail.com>
2009-05-22 21:14:23 +0000

    Update examples

--- examples/groups.py
+++ examples/groups.py
@@ -1,50 +1,68 @@
 
 """
-    Example demonstrating some features of groups in HDF5, and how to
-    use them from h5py.  
+    HDF5 for Python (h5py) is a Python interface to the HDF5 library.  Built
+    on a near-complete Python wrapping of the HDF5 C API, it exposes a simple,
+    NumPy-like interface for interacting with HDF5 files, datasets, attributes
+    and groups.
+
+    This module demonstrates the use of HDF5 groups from h5py.  HDF5 groups
+    are analagous to directories in a filesystem; they even use the UNIX-style
+    /path/to/resource syntax.  In h5py, groups act like dictionaries.  They
+    also provide the necessary methods to create subgroups, datasets, and
+    attributes.
+
+    HDF5 for Python is available at h5py.alfven.org.
 """
 
+import numpy as np
 import h5py
 
-groups = ['/foo', '/foo/bar', '/foo/bar/baz',
-          '/grp1', '/grp1/grp2', '/mygroup']
+f = h5py.File('myfile.hdf5','w')
 
-f = h5py.File('group_test.hdf5','w')
+# The file object is also the "root group" ("/") in HDF5.  It's currently
+# empty:
+print "Number of items in the root group: %d" % len(f)
 
-for grpname in groups:
-    f.create_group(grpname)
+# Create some groups
+g1 = f.create_group('Group1')
+g2 = f.create_group('Another Group')
+g3 = f.create_group('Yet another group')
 
-print "Root group names:"
+# All groups, including the root group, support a basic dictionary-style
+# interface
+print "There are now %d items in the root group" % len(f)
+print "They are: %s" % ", ".join(f)  # iterating yields member names
 
-for name in f:
-    print "   ", name
+# Groups can contain subgroups themselves
+sub1 = g1.create_group("Subgroup1")
 
-print "Root group info:"
+# Prints "/Group1/Subgroup1"
+print "Full name of subgroup is %s" % sub1.name
 
-for name, grp in f.iteritems():
-    print "    %s: %s items" % (name, len(grp))
+# You can retrieve them using __getitem__ syntax
+sub2 = g1['Subgroup1']
 
-if h5py.version.api_version_tuple >= (1,8):
+# You can attach attributes to groups, just like datasets, containing just
+# about anything NumPy can handle.
+g1.attrs['purpose'] = "A demonstration group"
+g1.attrs['Life, universe, everything'] = 42
+g1.attrs['A numpy array'] = np.ones((3,), dtype='>i2')
 
-    def treewalker(name):
-        """ Callback function for visit() """
-        print "    Called for %s" % name
+# Create datasets using group methods.  (See other examples for a more in-
+# depth introduction to datasets).
 
-    print "Walking..."
-    f.visit(treewalker)
+data = np.arange(100*100).reshape((100,100))
 
-    print "Copying /foo to /mygroup/newfoo..."
-    f.copy("/foo", "/mygroup/newfoo")
+dset = sub1.create_dataset("My dataset", data=data)
 
-    print "Walking again..."
-    f.visit(treewalker)
+print "The new dataset has full name %s, shape %s and type %s" % \
+        (dset.name, dset.shape, dset.dtype) 
 
-    g = f['/grp1']
-
-    print "Walking from /grp1..."
-    g.visit(treewalker)
-
-else:
-    print "HDF5 1.8 is needed for extra demos"
-
+# Closing the file closes all open objects
 f.close()
+
+
+
+
+
+
