13c7cff872ca9dfe4f7a6249ba3bd240bebd678b
andrewcollette <andrew.collette@gmail.com>
2010-01-05 06:45:30 +0000

    Fix windows DLL issue

--- setup.py
+++ setup.py
@@ -312,6 +312,7 @@ int main(){
                 fatal("Can't link against HDF5.")
             if sys.platform == 'win32':
                 shutil.copy(os.path.join(self.hdf5, 'dll', 'hdf5dll18.dll'), localpath('detect', 'hdf5dll18.dll'))
+                shutil.copy(os.path.join(self.hdf5, 'dll', 'zlib1.dll'), localpath('detect', 'zlib1.dll'))
             result = subprocess.Popen(localpath('detect', 'h5vers.exe'),
                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             so, se = result.communicate()
