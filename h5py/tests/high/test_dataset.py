a6a5819092d9d55ae16f823d3cd071e7ea6b2e67
andrewcollette <andrew.collette@gmail.com>
2010-03-09 23:34:56 +0000

    Add tests

--- h5py/tests/high/test_dataset.py
+++ h5py/tests/high/test_dataset.py
@@ -16,23 +16,6 @@ class Base(tests.HTest):
         self.f.close()
         os.unlink(self.name)
 
-class TestTypes(Base):
-
-    def test_array_dtype(self):
-        """ (Dataset) Array dtypes using non-tuple shapes """
-        dt1 = np.dtype('f4', (2,))
-        dt2 = np.dtype('f4', [2])
-        dt3 = np.dtype('f4', 2)
-        dt4 = np.dtype('f4', 2.1)
-        ds1 = self.f.create_dataset('ds1', (1,), dt1)
-        ds2 = self.f.create_dataset('ds2', (1,), dt2)
-        ds3 = self.f.create_dataset('ds3', (1,), dt3)
-        ds4 = self.f.create_dataset('ds4', (1,), dt4)
-        self.assertEqual(ds1.dtype, dt1)
-        self.assertEqual(ds2.dtype, dt1)
-        self.assertEqual(ds3.dtype, dt1)
-        self.assertEqual(ds4.dtype, dt1)
-
 class TestArray(Base):
 
     def test_array(self):
