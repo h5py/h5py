aca0d12fde4358a29533b9954de34a61fbfbdfe6
andrewcollette <andrew.collette@gmail.com>
2011-04-25 23:07:24 -0600

    Import setup.py from 1.3.1

---
+++ configure.py
@@ -0,0 +1,74 @@
+
+"""
+    Implements discovery of environment vars and command-line "superoptions".
+    
+    Also manages persistence via pickle files.
+
+    Currently defined settings (Key: Value):
+
+    "hdf5": <path to HDF5>
+"""
+
+import os, sys
+import warnings
+
+def loadpickle(name):
+    """ Load object from pickle file, or None if it can't be opened """
+    import pickle
+    try:
+        f = open(name,'r')
+    except IOError:
+        return None
+    try:
+        return pickle.load(f)
+    except Exception:
+        return None
+    finally:
+        f.close()
+
+def savepickle(name, data):
+    """ Save to pickle file, ignoring if it can't be written """
+    import pickle
+    try:
+        f = open(name, 'w')
+    except IOError:
+        return
+    try:
+        pickle.dump(data, f)
+    finally:
+        f.close()
+
+def scrape_eargs():
+    """ Locate settings in environment vars """
+    settings = {}
+
+    hdf5 = os.environ.get("HDF5_DIR", '')
+    if hdf5 != '':
+        settings['hdf5'] = hdf5
+
+    api = os.environ.get("HDF5_API", '')
+    if api != '':
+        warnings.warn("HDF5_API environment variable ignored (Support for HDF5 1.6 dropped)")
+
+    return settings
+
+def scrape_cargs():
+    """ Locate settings in command line or pickle file """
+    settings = loadpickle('h5py_config.pickle')
+    if settings is None:  settings = {}
+    for arg in sys.argv[:]:
+        if arg.find('--hdf5=') == 0:
+            hdf5 = arg.split('=')[-1]
+            if hdf5.lower() == 'default':
+                settings.pop('hdf5', None)
+            else:
+                settings['hdf5'] = hdf5
+            sys.argv.remove(arg)
+        if arg.find('--api=') == 0:
+            warnings.warn("--api option ignored (Support for HDF5 1.6 dropped)")
+            sys.argv.remove(arg)
+    savepickle('h5py_config.pickle', settings)
+    return settings
+
+
+    
--- setup.py
+++ setup.py
@@ -2,15 +2,56 @@ from distutils.core import setup
 from distutils.extension import Extension
 from distutils.cmd import Command
 from Cython.Distutils import build_ext
-import numpy
+import sys, os
 import os.path as op
+
+import numpy
+
+import configure
 
 VERSION = '1.4.0'
 
 def localpath(*args):
     return op.abspath(reduce(op.join, (op.dirname(__file__),)+args))
 
-modules = ['defs','_errors','_objects','_proxy', 'h5fd', 'h5z',
+
+# --- Determine HDF5 location -------------------------------------------------
+
+settings = configure.scrape_eargs()          # lowest priority
+settings.update(configure.scrape_cargs())    # highest priority
+
+HDF5 = settings.get('hdf5')
+
+
+# --- Create extensions -------------------------------------------------------
+
+if sys.platform.startswith('win'):
+    COMPILER_SETTINGS = {
+        'libraries'     : ['hdf5dll18'],
+        'include_dirs'  : [numpy.get_include(),  localpath('lzf'),
+                           localpath('win_include')],
+        'library_dirs'  : [],
+        'define_macros' : [('H5_USE_16_API', None), ('_HDF5USEDLL_', None)]
+    }
+    if HDF5 is not None:
+        COMPILER_SETTINGS['include_dirs'] += [op.join(HDF5, 'include')]
+        COMPILER_SETTINGS['library_dirs'] += [op.join(HDF5, 'dll')]
+else:
+    COMPILER_SETTINGS = {
+       'libraries'      : ['hdf5'],
+       'include_dirs'   : [numpy.get_include(), localpath('lzf')],
+       'library_dirs'   : [],
+       'define_macros'  : [('H5_USE_16_API', None)]
+    }
+    if HDF5 is not None:
+        COMPILER_SETTINGS['include_dirs'] += [op.join(HDF5, 'include')]
+        COMPILER_SETTINGS['library_dirs'] += [op.join(HDF5, 'lib')]
+    elif sys.platform == 'darwin':
+        COMPILER_SETTINGS['include_dirs'] += ['/opt/local/include']
+        COMPILER_SETTINGS['library_dirs'] += ['/opt/local/lib']
+    COMPILER_SETTINGS['runtime_library_dirs'] = [op.abspath(x) for x in COMPILER_SETTINGS['library_dirs']]
+
+MODULES =  ['defs','_errors','_objects','_proxy', 'h5fd', 'h5z',
             'h5','h5i','h5r','utils',
             '_conv', 'h5t','h5s',
             'h5p',
@@ -18,21 +59,26 @@ modules = ['defs','_errors','_objects','_proxy', 'h5fd', 'h5z',
             'h5l', 'h5o']
 
 EXTRA_SRC = {'h5z': [ localpath("lzf/lzf_filter.c"), 
-                     localpath("lzf/lzf/lzf_c.c"),
-                     localpath("lzf/lzf/lzf_d.c")]}
+                      localpath("lzf/lzf/lzf_c.c"),
+                      localpath("lzf/lzf/lzf_d.c")]}
 
-ext_modules = [ Extension('h5py.%s' %x,
-                ['h5py/%s.pyx' % x] + EXTRA_SRC.get(x,[]),
-                include_dirs = [localpath('lzf'), numpy.get_include()],
-                libraries = ['hdf5'],
-                define_macros = [('H5_USE_16_API',None)],
-) for x in modules]
+def make_extension(module):
+    sources = [op.join('h5py', module+'.pyx')] + EXTRA_SRC.get(module, [])
+    return Extension('h5py.'+module, sources, **COMPILER_SETTINGS)
+
+EXTENSIONS = [make_extension(m) for m in MODULES]
+
+
+# --- Write PXI file (this should go away soon) -------------------------------
 
 with open('h5py/config.pxi','w') as config_file:
     config_file.write('DEF H5PY_16API=1\n')
     config_file.write('DEF H5PY_18API=1\n')
     config_file.write('DEF H5PY_API=18\n')
     config_file.write('DEF H5PY_VERSION="%s"\n'%VERSION)
+
+
+# --- Custom distutils commands -----------------------------------------------
 
 class test(Command):
 
@@ -68,9 +114,65 @@ class test(Command):
         suite = unittest.TestLoader().discover('.')
         unittest.TextTestRunner(verbosity=self.verbosity+1).run(suite)
 
+
+# --- Distutils setup and metadata --------------------------------------------
+
+cls_txt = \
+"""
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+Intended Audience :: Information Technology
+Intended Audience :: Science/Research
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Topic :: Database
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Unix
+Operating System :: POSIX :: Linux
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
+"""
+
+short_desc = "Read and write HDF5 files from Python"
+
+long_desc = \
+"""
+The h5py package provides both a high- and low-level interface to the HDF5
+library from Python. The low-level interface is intended to be a complete
+wrapping of the HDF5 API, while the high-level component supports  access to
+HDF5 files, datasets and groups using established Python and NumPy concepts.
+
+A strong emphasis on automatic conversion between Python (Numpy) datatypes and
+data structures and their HDF5 equivalents vastly simplifies the process of
+reading and writing data from Python.
+
+Supports HDF5 versions 1.8.2 and higher.  On Windows, HDF5 is included with
+the installer.
+"""
+
+if os.name == 'nt':
+    package_data = {'h5py': ['*.pyx', '*.dll']}
+else:
+    package_data = {'h5py': ['*.pyx']}
+
 setup(
-    cmdclass = {'build_ext': build_ext, 'test': test},
-    ext_modules = ext_modules,
-    packages = ['h5py', 'h5py._hl', 'h5py._hl.tests', 'h5py.lowtest']
+  name = 'h5py',
+  version = VERSION if not sys.platform.startswith('win') else VERSION.partition('-')[0],
+  description = short_desc,
+  long_description = long_desc,
+  classifiers = [x for x in cls_txt.split("\n") if x],
+  author = 'Andrew Collette',
+  author_email = '"h5py" at the domain "alfven.org"',
+  maintainer = 'Andrew Collette',
+  maintainer_email = '"h5py" at the domain "alfven.org"',
+  url = 'http://h5py.alfven.org',
+  download_url = 'http://code.google.com/p/h5py/downloads/list',
+  packages = ['h5py', 'h5py._hl', 'h5py._hl.tests', 'h5py.lowtest'],
+  package_data = package_data,
+  ext_modules = EXTENSIONS,
+  requires = ['numpy (>=1.0.1)'],
+  cmdclass = {'build_ext': build_ext, 'test': test}
 )
+
 
