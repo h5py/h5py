6f71b11d7f75861b3cbc183ccfdf960d3e2b28a1
andrewcollette <andrew.collette@gmail.com>
2011-05-22 16:08:56 -0600

    Make selections module non-public

--- h5py/highlevel.py
+++ h5py/highlevel.py
@@ -1,4 +1,4 @@
-from _hl import filters, selections
+from _hl import filters
 from _hl.base import is_hdf5, HLObject
 from _hl.files import File
 from _hl.group import Group, SoftLink, ExternalLink, HardLink
