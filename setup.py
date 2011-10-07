93a19239856ef8756d36ed245f7b606bd4f887e0
andrewcollette <andrew.collette@gmail.com>
2011-10-06 19:42:48 -0600

    Add version check for cython (issue 221)

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ import os.path as op
 from functools import reduce
 
 try:
+    import Cython.Compiler.Version
+    vers = [int(x) for x in Cython.Compiler.Version.version.split('.')]
+    if vers < (0,13):
+        raise ImportError
     from Cython.Distutils import build_ext
     SUFFIX = '.pyx'
 except ImportError:
