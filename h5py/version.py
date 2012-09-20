c3f090f3123679e072c3bb6633f451f5f1f780a9
andrewcollette <andrew.collette@gmail.com>
2012-09-19 18:50:12 -0600

    update version number to 2.2.0 on default branch

--- h5py/version.py
+++ h5py/version.py
@@ -1,7 +1,7 @@
 from . import h5 as _h5
 from distutils.version import StrictVersion as _sv
 
-version = "2.1.0b2"
+version = "2.2.0a"
 _exp = _sv(version)
 
 version_tuple = _exp.version + (''.join(str(x) for x in _exp.prerelease),)
