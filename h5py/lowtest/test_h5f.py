22c50761966ec4cf3c060b32716e9f7fe77ebd74
Thomas A Caswell <tcaswell@uchicago.edu>
2013-06-12 20:10:16 -0500

    added basic test for the cache config functionality

--- h5py/lowtest/test_h5f.py
+++ h5py/lowtest/test_h5f.py
@@ -25,3 +25,39 @@ class TestFileID(ut.TestCase):
                 os.fsync(descriptor)
         finally:
             shutil.rmtree(dn_tmp)
+
+
+class TestCacheConfig(ut.TestCase):
+    def test_simple_gets(self):
+        dn_tmp = tempfile.mkdtemp('h5py.lowtest.test_h5f.TestFileID.TestCacheConfig.test_simple_gets')
+        fn_h5 = os.path.join(dn_tmp, 'test.h5')
+        try:
+            with File(fn_h5) as f:
+                hit_rate = f._id.get_mdc_hit_rate()
+                mdc_size = f._id.get_mdc_size()
+
+        finally:
+            shutil.rmtree(dn_tmp)
+
+    def test_hitrate_reset(self):
+        dn_tmp = tempfile.mkdtemp('h5py.lowtest.test_h5f.TestFileID.TestCacheConfig.test_hitrate_reset')
+        fn_h5 = os.path.join(dn_tmp, 'test.h5')
+        try:
+            with File(fn_h5) as f:
+                hit_rate = f._id.get_mdc_hit_rate()
+                f._id.reset_mdc_hit_rate_stats()
+                hit_rate = f._id.get_mdc_hit_rate()
+                assert hit_rate == 0
+
+        finally:
+            shutil.rmtree(dn_tmp)
+
+    def test_mdc_config_get(self):
+        dn_tmp = tempfile.mkdtemp('h5py.lowtest.test_h5f.TestFileID.TestCacheConfig.test_mdc_config_get')
+        fn_h5 = os.path.join(dn_tmp, 'test.h5')
+        try:
+            with File(fn_h5) as f:
+                conf = f._id.get_mdc_config()
+                f._id.set_mdc_config(conf)
+        finally:
+            shutil.rmtree(dn_tmp)
