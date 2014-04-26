c28178b5df97221394cf21d3a66213a04f8b5863
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-04-26 22:07:09 +0200

    Adding tests for mdc_config

--- h5py/tests/test_h5p.py
+++ h5py/tests/test_h5p.py
@@ -32,7 +32,7 @@ class TestDA(ut.TestCase):
     '''
     Feature: setting/getting chunk cache size on a dataset access property list
     '''
-    def test_chuck_cache(self):
+    def test_chunk_cache(self):
         '''test get/set chunk cache '''
         dalist = h5p.create(h5p.DATASET_ACCESS)
         nslots = 10000 # 40kb hash table
@@ -42,6 +42,18 @@ class TestDA(ut.TestCase):
         dalist.set_chunk_cache(nslots, nbytes, w0)
         self.assertEqual((nslots, nbytes, w0),
                          dalist.get_chunk_cache())
+
+class TestFA(ut.TestCase):
+  '''
+    Feature: setting/getting mdc config on a file access property list
+    '''
+  def test_mdc_config(self):
+    '''test get/set mdc config '''
+    falist = h5p.create(h5p.FILE_ACCESS)
+    
+    config = falist.get_mdc_config()
+    falist.set_mdc_config(config)
+
 
 class TestPL(ut.TestCase):
     def test_obj_track_times(self):
