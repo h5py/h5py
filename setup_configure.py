60b947d046a152b72116108f444d0b96f5d8253a
Alex Ford <fordas@uw.edu>
2016-05-13 23:11:54 +0000

    Fix pkgconfig handling in setup_configure autodetect_version.
    pkgconfig.parse may return a collection of library paths, rather than a string
    library path. Fix pkgconfig handling in order to properly search
    provided library paths, which is the base case for debian libhdf5-dev
    installs.

--- setup_configure.py
+++ setup_configure.py
@@ -204,7 +204,11 @@ def autodetect_version(hdf5_dir=None):
     libdirs = ['/usr/local/lib', '/opt/local/lib']
     try:
         if pkgconfig.exists("hdf5"):
-            libdirs.append(pkgconfig.parse("hdf5")['library_dirs'])
+            hdf5_library_dirs = pkgconfig.parse("hdf5")['library_dirs']
+            if isinstance(hdf5_library_dirs, basestring):
+                libdirs.append(hdf5_library_dirs)
+            else:
+                libdirs.extend(hdf5_library_dirs)
     except EnvironmentError:
         pass
     if hdf5_dir is not None:
