bec380096fc603861060af285783c01733fb3788
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-02-20 11:40:27 +0000

    fixes an edge bug

--- h5py/tests/hl/vds_tests/test_virtual_source.py
+++ h5py/tests/hl/vds_tests/test_virtual_source.py
@@ -17,6 +17,11 @@ class TestVirtualSource(unittest.TestCase):
         dataset = h5.VirtualSource('test','test',(20,30,30))
         sliced = dataset[5,:,:]
         self.assertEqual((1,30,30),sliced.shape)
+
+    def test_integer_single_indexed(self):
+        dataset = h5.VirtualSource('test','test',(20,30,30))
+        sliced = dataset[5]
+        self.assertEqual((1,30,30),sliced.shape)
     
     def test_two_integer_indexed(self):
         dataset = h5.VirtualSource('test','test',(20,30,30))
