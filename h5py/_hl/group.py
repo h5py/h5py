aae864330c247bd9707abd9fa4580b5da8fa4c31
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-02-20 12:51:27 +0000

    corrects an error with the slice list

--- h5py/_hl/group.py
+++ h5py/_hl/group.py
@@ -26,6 +26,7 @@ from . import base
 from .base import HLObject, MutableMappingHDF5, phil, with_phil
 from . import dataset
 from . import datatype
+
 # from . import files
 # # from .files import File
 
@@ -598,7 +599,7 @@ class DatasetContainer(object):
         self.path = path
         self.key = key
         self.shape = shape
-        self.slice_list = [slice(None, None, None)] * len(self.shape) # if we don't slice, we want the whole array
+        self.slice_list = [slice(0, ix, 1) for ix in self.shape] # if we don't slice, we want the whole array
         if maxshape is None:
             self.maxshape=shape
         else:
@@ -708,9 +709,10 @@ class VirtualMap(object):
             self.block_shape = self.src.shape
 
         self.src_dspace = h5s.create_simple(self.src.shape, self.src.maxshape)
-        start_idx = tuple([0 if ix.start is None else ix.start for ix in self.src.slice_list])
-        stride_idx = tuple([1 if ix.step is None else ix.step for ix in self.src.slice_list])
-        
+
+        start_idx = tuple([ix.start for ix in self.src.slice_list])
+        stride_idx = tuple([ix.step for ix in self.src.slice_list])
+
         if any(ix==h5s.UNLIMITED for ix in self.src.maxshape):
             count_idx = [1, ] * len(stride_idx)
             unlimited_index = self.src.maxshape.index(h5s.UNLIMITED)
