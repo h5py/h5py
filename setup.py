c6af3066e3a5b56808ff84cfca353152fdeccfb9
Felix Yan <felixonmars@archlinux.org>
2017-03-22 16:40:26 +0800

    Note explicit support for Python 3.6

--- setup.py
+++ setup.py
@@ -113,6 +113,7 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Scientific/Engineering
 Topic :: Database
