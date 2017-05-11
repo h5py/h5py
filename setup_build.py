1f5c7c38136ff27a6079bf1b1b23f85054533a5d
Paco Hope <pacohope@amazon.co.uk>
2017-05-11 10:35:55 +0100

    Added missing commas.

--- setup_build.py
+++ setup_build.py
@@ -49,8 +49,8 @@ if sys.platform.startswith('win'):
         ('H5_BUILT_AS_DYNAMIC_LIB', None)
     ])
 else:
-    COMPILER_SETTINGS['include_dirs'].extend(['/opt/local/include''/usr/local/include'])
-    COMPILER_SETTINGS['library_dirs'].extend(['/opt/local/include''/usr/local/include'])
+    COMPILER_SETTINGS['include_dirs'].extend(['/opt/local/include','/usr/local/include'])
+    COMPILER_SETTINGS['library_dirs'].extend(['/opt/local/include','/usr/local/include'])
 
 
 class h5py_build_ext(build_ext):
