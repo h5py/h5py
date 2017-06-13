fe3b6b89523f500ce84751de4e03b4d95f3a601d
Thomas A Caswell <tcaswell@gmail.com>
2017-06-13 18:49:18 -0400

    MNT: make VDS support conditional
    Only expose the VDS related classes and method if VDS is available
    
     - move VDS classes to separate module
     - put Group.create_virtual behind a version guard
     - put top-level imports behind version guard

--- h5py/__init__.py
+++ h5py/__init__.py
@@ -52,7 +52,7 @@ from ._hl.files import (
     unregister_driver,
     registered_drivers,
 )
-from ._hl.group import Group, SoftLink, ExternalLink, HardLink, VirtualSource, VirtualTarget, VirtualMap
+from ._hl.group import Group, SoftLink, ExternalLink, HardLink
 from ._hl.dataset import Dataset
 from ._hl.datatype import Datatype
 from ._hl.attrs import AttributeManager
@@ -65,9 +65,12 @@ from . import version
 from .version import version as __version__
 
 
+if version.hdf5_version_tuple[:3] >= get_config().vds_min_hdf5_version:
+    from ._hl.vds import VirtualSource, VirtualTarget, VirtualMap
+
 if version.hdf5_version_tuple != version.hdf5_built_version_tuple:
     _warn(("h5py is running against HDF5 {0} when it was built against {1}, "
-        "this may cause problems").format(
+           "this may cause problems").format(
             '{0}.{1}.{2}'.format(*version.hdf5_version_tuple),
             '{0}.{1}.{2}'.format(*version.hdf5_built_version_tuple)
     ))
