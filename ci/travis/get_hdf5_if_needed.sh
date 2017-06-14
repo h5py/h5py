bf70c6ec93c69cdb9f6f67f4f046222f84b8f1d3
Thomas A Caswell <tcaswell@gmail.com>
2017-06-14 18:50:48 -0400

    CI: use bash

--- ci/travis/get_hdf5_if_needed.sh
+++ ci/travis/get_hdf5_if_needed.sh
@@ -6,6 +6,16 @@ if [ -z ${HDF5_DIR+x} ]; then
     echo "Using OS HDF5"
 else
     echo "Using downloaded HDF5"
-    python3 -m pip install requests
-    python3 ci/get_hdf5.py
+    #python3 -m pip install requests
+    #python3 ci/get_hdf5.py
+    pushd /tmp
+    wget https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz
+    tar -xzvf hdf5-$HDF5_VERSION.tar.gz
+    pushd hdf5-$HDF5_VERSION
+    chmod u+x autogen.sh
+    ./configure --prefix $HDF5_DIR
+    make -j 2
+    make install
+    popd
+    popd
 fi
