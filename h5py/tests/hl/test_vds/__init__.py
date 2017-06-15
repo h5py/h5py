93e8fa5141dff7d66730d0ba023b8badc88fe874
Thomas A Caswell <tcaswell@gmail.com>
2017-06-14 20:01:53 -0400

    TST: restructure tests and skip VDS tests on old hdf5

--- h5py/tests/hl/test_vds/__init__.py
+++ h5py/tests/hl/test_vds/__init__.py
@@ -1,9 +1,6 @@
 from __future__ import absolute_import
 
-from .test_eiger_high_level import *
-from .test_eiger_low_level import *
-from .test_excalibur_high_level import *
-from .test_excalibur_low_level import *
-from .test_percival_high_level import *
-from .test_percival_low_level import *
+
 from .test_virtual_source import *
+from .test_highlevel_vds import *
+from .test_lowlevel_vds import *
