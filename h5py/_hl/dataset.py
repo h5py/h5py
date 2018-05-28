95fbbaaabcb0c7ca6f60d3b50a55049e572d2c51
Valentin Valls <valentin.valls@esrf.fr>
2018-05-28 12:54:30 +0200

    Fix empty dataset size

--- h5py/_hl/dataset.py
+++ h5py/_hl/dataset.py
@@ -235,6 +235,8 @@ class Dataset(HLObject):
     @with_phil
     def size(self):
         """Numpy-style attribute giving the total dataset size"""
+        if is_empty_dataspace(self.id):
+            return None
         return numpy.prod(self.shape, dtype=numpy.intp)
 
     @property
