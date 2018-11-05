4971c7e8f7a74109d8722538a44cadcb5b724010
Chris Billington <chrisjbillington@gmail.com>
2018-11-05 13:45:31 -0500

    Only auto-convert lists and tuples

--- h5py/_hl/attrs.py
+++ h5py/_hl/attrs.py
@@ -119,14 +119,12 @@ class AttributeManager(base.MutableMappingHDF5, base.CommonStateObject):
             # First, make sure we have a NumPy array.  We leave the data type
             # conversion for HDF5 to perform (other than the below exception).
             if not isinstance(data, Empty):
-                # Whether we were already given a numpy array or whether we are
-                # converting to a numpy array ourselves:
-                auto_convert_to_array = not isinstance(data, numpy.ndarray)
+                is_list_or_tuple = isinstance(data, (list, tuple))
                 data = numpy.asarray(data, order='C')
-                # If we converted to a numpy array ourselves, then we do not
-                # need to respect the datatype of the numpy array. If it is U
-                # type, convert to vlen unicode strings:
-                if auto_convert_to_array and data.dtype.type == numpy.unicode_:
+                # If we were passed a list or tuple, then we do not need to respect the
+                # datatype of the numpy array. If it is U type, convert to vlen unicode
+                # strings:
+                if is_list_or_tuple and data.dtype.type == numpy.unicode_:
                     data = numpy.array(data, dtype=h5t.special_dtype(vlen=six.text_type))
 
             if shape is None:
