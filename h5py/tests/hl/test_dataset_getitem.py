5d44049411d90dd7dd96d4185d0677a60a0c0bd8
Thomas Kluyver <thomas@kluyver.me.uk>
2019-02-14 14:52:00 +0000

    Add some tests for fancy indexing on a 2D dataset

--- h5py/tests/hl/test_dataset_getitem.py
+++ h5py/tests/hl/test_dataset_getitem.py
@@ -471,6 +471,30 @@ class Test2DZeroFloat(TestCase):
         self.assertNumpyBehavior(self.dset, self.data, np.s_[:,[0,1,2]])
 
 
+class Test2DFloat(TestCase):
+
+    def setUp(self):
+        TestCase.setUp(self)
+        self.data = np.ones((5,3), dtype='f')
+        self.dset = self.f.create_dataset('x', data=self.data)
+
+    def test_ndim(self):
+        """ Verify number of dimensions """
+        self.assertEqual(self.dset.ndim, 2)
+
+    def test_shape(self):
+        """ Verify shape """
+        self.assertEqual(self.dset.shape, (5, 3))
+
+    def test_indexlist(self):
+        """ see issue #473 """
+        self.assertNumpyBehavior(self.dset, self.data, np.s_[:,[0,1,2]])
+
+    def test_index_emptylist(self):
+        self.assertNumpyBehavior(self.dset, self.data, np.s_[:, []])
+        self.assertNumpyBehavior(self.dset, self.data, np.s_[[]])
+
+
 class TestVeryLargeArray(TestCase):
 
     def setUp(self):
