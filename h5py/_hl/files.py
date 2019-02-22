475303e65d892a5dab714fd130cd690fe54c1d5a
Thomas Kluyver <thomas@kluyver.me.uk>
2019-02-22 09:07:06 +0000

    Add H5PY_DEFAULT_READONLY environment variable

--- h5py/_hl/files.py
+++ h5py/_hl/files.py
@@ -185,7 +185,8 @@ def make_fid(name, mode, userblock_size, fapl, fcpl=None, swmr=False):
     elif mode is None:
         warn("The default file mode will change to 'r' (read-only) in h5py 3.0. "
              "To suppress this warning, pass the mode you need to h5py.File(), "
-             "or set the global default h5.get_config().default_file_mode. "
+             "or set the global default h5.get_config().default_file_mode, "
+             "or set the environment variable H5PY_DEFAULT_READONLY=1. "
              "Available modes are: 'r', 'r+', 'w', 'w-'/'x', 'a'. "
              "See the docs for details.", H5pyDeprecationWarning, stacklevel=3)
         # Try to open in append mode (read/write).
@@ -393,6 +394,8 @@ class File(Group):
                 track_order = h5.get_config().track_order
             if mode is None:
                 mode = h5.get_config().default_file_mode
+                if mode is None and os.environ.get('H5PY_DEFAULT_READONLY', ''):
+                    mode = 'r'
 
             with phil:
                 fapl = make_fapl(driver, libver, rdcc_nslots, rdcc_nbytes, rdcc_w0, **kwds)
