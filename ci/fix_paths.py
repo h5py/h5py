f9c141106ebf4787bc96c38a8596850aac47acd7
Thomas Kluyver <thomas@kluyver.me.uk>
2019-04-01 12:33:43 +0100

    Look up site-packages dir inside each environment

--- ci/fix_paths.py
+++ ci/fix_paths.py
@@ -1,4 +1,4 @@
-import argparse
+import distutils.sysconfig
 from glob import glob
 from os import environ
 from os.path import join as pjoin
@@ -7,15 +7,15 @@ from sys import platform
 
 def main():
     """
-    Fix paths to dlls
+    Copy HDF5 DLLs into installed h5py package
     """
-    p = argparse.ArgumentParser()
-    p.add_argument("sitepackagesdir")
-    args = p.parse_args()
+    # This is the function Tox also uses to locate site-packages (Apr 2019)
+    sitepackagesdir = distutils.sysconfig.get_python_lib()
+
     hdf5_path = environ.get("HDF5_DIR")
     if platform.startswith('win'):
         for f in glob(pjoin(hdf5_path, 'lib/*.dll')):
-            copy(f, pjoin(args.sitepackagesdir, 'h5py'))
+            copy(f, pjoin(sitepackagesdir, 'h5py'))
 
 
 if __name__ == '__main__':
