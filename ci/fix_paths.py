0a8179565be37804fc3c1d77c97024648cdc68d6
Thomas Kluyver <thomas@kluyver.me.uk>
2019-06-03 19:48:12 +0100

    Inspect where & what h5py is installed
    More diagnostics in fix_paths.py
    
    Diagnostics, robustness in fix_paths.py

--- ci/fix_paths.py
+++ ci/fix_paths.py
@@ -1,7 +1,9 @@
+from __future__ import print_function
+
 import distutils.sysconfig
 from glob import glob
-from os import environ
-from os.path import join as pjoin
+import os
+from os.path import join as pjoin, basename
 from shutil import copy
 from sys import platform
 
@@ -11,12 +13,17 @@ def main():
     """
     # This is the function Tox also uses to locate site-packages (Apr 2019)
     sitepackagesdir = distutils.sysconfig.get_python_lib()
+    print("site packages dir:", sitepackagesdir)
 
-    hdf5_path = environ.get("HDF5_DIR")
+    hdf5_path = os.environ.get("HDF5_DIR")
+    print("HDF5_DIR", hdf5_path)
+
     if platform.startswith('win'):
         for f in glob(pjoin(hdf5_path, 'lib/*.dll')):
-            copy(f, pjoin(sitepackagesdir, 'h5py'))
+            copy(f, pjoin(sitepackagesdir, 'h5py', basename(f)))
+            print("Copied", f)
 
+    print("In installed h5py:", os.listdir(pjoin(sitepackagesdir, 'h5py')))
 
 if __name__ == '__main__':
     main()
