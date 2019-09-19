af698c86fc9e4a57de90a737a1d246be660996e8
Thomas A Caswell <tcaswell@bnl.gov>
2019-09-20 00:44:58 +0200

    TST: add a test

--- h5py/tests/test_dataset.py
+++ h5py/tests/test_dataset.py
@@ -1196,3 +1196,33 @@ def test_hide_value_from_jedi():
         with pytest.warns(UserWarning):
             assert hasattr(fout['test'], 'value')
         assert 'value' not in dir(fout['test'])
+
+
+def test_get_chunk_details():
+    from io import BytesIO
+    buf = BytesIO()
+    with h5py.File(buf, 'w') as fout:
+        fout.create_dataset('test', shape=(100, 100), chunks=(10, 10), dtype='i4')
+        fout['test'][:] = 1
+
+    buf.seek(0)
+    with h5py.File(buf, 'r') as fin:
+        ds = fin['test'].id
+
+        assert ds.get_num_chunks() == 100
+        for j in range(100):
+            offset = tuple(np.array(np.unravel_index(j, (10, 10))) * 10)
+
+            si = ds.get_chunk_info(j)
+            assert si.index == j
+            assert si.chunk_offset == offset
+            assert si.filter_mask == 0
+            assert si.byte_offset is not None
+            assert si.size > 0
+
+        si = ds.get_chunk_info_by_coord((0, 0))
+        assert si.index is None
+        assert si.chunk_offset == (0, 0)
+        assert si.filter_mask == 0
+        assert si.byte_offset is not None
+        assert si.size > 0
