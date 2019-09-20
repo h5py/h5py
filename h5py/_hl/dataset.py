38f48aceca8f93699cb346ba8d48f7dcb7807874
John Readey <jreadey@hdfgroup.org>
2019-09-20 17:07:52 +0200

    Added chunk iteratation class

--- h5py/_hl/dataset.py
+++ h5py/_hl/dataset.py
@@ -242,7 +242,88 @@ if MPI:
         def __exit__(self, *args):
             # pylint: disable=protected-access
             self._dset._dxpl.set_dxpl_mpio(h5fd.MPIO_INDEPENDENT)
+class ChunkIterator(object):
+    """
+    Class to iterate through list of chunks of a given dataset
+    # Note: Only works with PY3 since as is doesn't have a "next = __next__" redef
+    """
+    def __init__(self, dset, source_sel=None):
+        self._shape = dset.shape
+        rank = len(dset.shape)
 
+        if dset.chunks is None:
+            # treat the dataset as one chunk
+            self._layout = dset.shape
+        else:
+            self._layout = dset.chunks
+        if source_sel is None:
+            # select over entire dataset
+            slices = []
+            for dim in range(rank):
+                slices.append(slice(0, self._shape[dim]))
+            self._sel = tuple(slices)
+        else:
+            if isinstance(source_sel, slice):
+                self._sel = (source_sel,)
+            else:
+                # tbd - check that this is a list or tuple or slices?
+                self._sel = source_sel
+        if len(self._sel) != rank:
+            # TBD: how does h5py handle these type of errors?
+            raise ValueError("Invalid selection")
+        self._chunk_index = []
+        for dim in range(rank):
+            s = self._sel[dim]
+            if s.start < 0 or s.stop > self._shape[dim] or s.stop <= s.start:
+                raise ValueError("Invalid selection")
+            index = s.start // self._layout[dim]
+            self._chunk_index.append(index)
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        def get_ret(item):
+            if len(item) == 1:
+                return item[0]
+            else:
+                return tuple(item)
+        rank = len(self._shape)
+        slices = []
+        if rank == 0 or self._chunk_index[0] * self._layout[0] >= self._sel[0].stop:
+            # ran past the last chunk, end iteration
+            raise StopIteration()
+
+        for dim in range(rank):
+            s = self._sel[dim]
+            start = self._chunk_index[dim] * self._layout[dim]
+            stop = (self._chunk_index[dim] + 1) * self._layout[dim]
+            # adjust the start if this is an edge chunk
+            if start < s.start:
+                start = s.start
+            if stop > s.stop:
+                stop = s.stop  # trim to end of the selection
+            s = slice(start, stop, 1)
+            slices.append(s)
+
+        # bump up the last index and carry forward if we run outside the selection
+        dim = rank - 1
+        while dim >= 0:
+            s = self._sel[dim]
+            self._chunk_index[dim] += 1
+
+            chunk_end = self._chunk_index[dim] * self._layout[dim]
+            if chunk_end < s.stop:
+                # we still have room to extend along this dimensions
+                return get_ret(slices)
+
+            if dim > 0:
+                # reset to the start and continue iterating with higher dimension
+                self._chunk_index[dim] = 0
+            dim -= 1
+        return get_ret(slices)
 
 class Dataset(HLObject):
 
