b699de2b7cec0dd809f5b705da960d52f788927d
Julia Garriga Ferrer <julia.garriga@esrf.fr>
2019-09-25 11:42:02 +0200

    [integer shapes] VirtualLayout maxshape to tuple if integer

--- h5py/_hl/vds.py
+++ h5py/_hl/vds.py
@@ -123,9 +123,9 @@ class VirtualLayout(object):
         axes you want to be unlimited.
     """
     def __init__(self, shape, dtype=None, maxshape=None):
-        self.shape = (shape,) if isinstance(shape,int) else shape
+        self.shape = (shape,) if isinstance(shape, int) else shape
         self.dtype = dtype
-        self.maxshape = maxshape
+        self.maxshape = (maxshape,) if isinstance(maxshape, int) else maxshape
         self.sources = []
 
     def __setitem__(self, key, source):
