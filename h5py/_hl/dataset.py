05ac82a4e982c06ae3e344f5fb9135cfb9d3efcf
Pierlauro Sciarelli <foss@pstux.dev>
2019-11-01 23:32:01 +0000

    Partial revert "Updated docstrings"
    This reverts commit ee1c4ca4406721b4c9d0f03ab909587cc69aa1c8.

--- h5py/_hl/dataset.py
+++ h5py/_hl/dataset.py
@@ -53,7 +53,10 @@ def make_new_dset(parent, shape=None, dtype=None, data=None, name=None,
                   fletcher32=None, maxshape=None, compression_opts=None,
                   fillvalue=None, scaleoffset=None, track_times=None,
                   external=None, track_order=None, dcpl=None):
-    """ Return a new low-level dataset identifier """
+    """ Return a new low-level dataset identifier
+
+    Only creates anonymous datasets.
+    """
 
     # Convert data to a C-contiguous ndarray
     if data is not None and not isinstance(data, Empty):
@@ -169,8 +172,11 @@ def make_new_dset(parent, shape=None, dtype=None, data=None, name=None,
 
 def make_new_virtual_dset(parent, shape, sources, dtype=None,
                           maxshape=None, fillvalue=None):
-    """ Return a new low-level dataset identifier for a virtual dataset """
+    """Return a new low-level dataset identifier for a virtual dataset
 
+    Like make_new_dset(), this creates an anonymous dataset, which can be given
+    a name later.
+    """
     # create the creation property list
     dcpl = h5p.create(h5p.DATASET_CREATE)
     if fillvalue is not None:
