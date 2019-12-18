9e36444a7537b4a804b06ca743b2000ca302b657
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-18 19:23:32 +0000

    Update docs after building Travis build 1941 of h5py/h5py
    The docs were built from the branch 'master' against the commit
    d3a8d797d0754aded92a6f0087e4579c1709be17.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/h5py/h5py/jobs/626854722.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy . --built-docs docs_api/_build/html

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '2.10.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
