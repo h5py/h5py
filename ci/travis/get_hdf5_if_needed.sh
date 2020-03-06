e69a9cbaddfc34aac4a75fa785f0f0600e579b86
Thomas Kluyver <thomas@kluyver.me.uk>
2020-03-06 10:57:36 +0000

    Fix HDF5 source URL for versions other than 1.10.*

--- ci/travis/get_hdf5_if_needed.sh
+++ ci/travis/get_hdf5_if_needed.sh
@@ -12,7 +12,8 @@ else
 	echo "using cached build"
     else
         pushd /tmp
-        wget https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz
+        #                             Remove trailing .*, to get e.g. '1.12' ↓
+        wget "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
         tar -xzvf hdf5-$HDF5_VERSION.tar.gz
         pushd hdf5-$HDF5_VERSION
         chmod u+x autogen.sh
