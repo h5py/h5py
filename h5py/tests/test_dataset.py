bba57113a66f99ef2cf365c30e3eb8231d1986b0
Thomas Kluyver <thomas@kluyver.me.uk>
2020-06-26 16:23:24 +0100

    Add failing test for writing to uint64 attribute

--- h5py/tests/test_dataset.py
+++ h5py/tests/test_dataset.py
@@ -1454,7 +1454,7 @@ def test_zero_storage_size():
         assert fin['empty'].id.get_storage_size() == 0
 
 
-def test_create_uint64(writable_file):
+def test_python_int_uint64(writable_file):
     # https://github.com/h5py/h5py/issues/1547
     data = [np.iinfo(np.int64).max, np.iinfo(np.int64).max + 1]
 
