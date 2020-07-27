14e5542db20f1bbcdadd0a4d6a70fc05b191ceba
Chen <chenguopingdota@163.com>
2020-07-27 21:26:57 +0800

    Clean some code (#1590)
    * clean some code
    
    * update
    
    * update files.py
    
    * Update h5py/_hl/files.py
    
    Co-authored-by: Thomas Kluyver <takowl@gmail.com>
    
    * revert the changes in make_fcpl() method
    
    Co-authored-by: Thomas Kluyver <takowl@gmail.com>

--- h5py/_hl/dataset.py
+++ h5py/_hl/dataset.py
@@ -113,10 +113,10 @@ def make_new_dset(parent, shape=None, dtype=None, data=None, name=None,
         dcpl.set_obj_track_times(track_times)
     elif track_times is not None:
         raise TypeError("track_times must be either True or False")
-    if track_order == True:
+    if track_order is True:
         dcpl.set_attr_creation_order(
             h5p.CRT_ORDER_TRACKED | h5p.CRT_ORDER_INDEXED)
-    elif track_order == False:
+    elif track_order is False:
         dcpl.set_attr_creation_order(0)
     elif track_order is not None:
         raise TypeError("track_order must be either True or False")
