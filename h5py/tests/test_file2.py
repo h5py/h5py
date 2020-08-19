4da124de867695dd1490858e4365cd0b02422e5b
Thomas Kluyver <thomas@kluyver.me.uk>
2020-08-19 15:27:59 +0100

    Close file in test after failing to write to it

--- h5py/tests/test_file2.py
+++ h5py/tests/test_file2.py
@@ -208,13 +208,22 @@ class TestFileObj(TestCase):
     def test_exception_write(self):
 
         class BrokenBytesIO(io.BytesIO):
+            allow_write = False
             def write(self, b):
-                raise Exception('I am broken')
+                if self.allow_write:
+                    return super().write(b)
+                else:
+                    raise Exception('I am broken')
 
-        f = h5py.File(BrokenBytesIO(), 'w')
-        self.assertRaises(Exception, f.create_dataset, 'test',
-                          data=list(range(12)))
-        self.assertRaises(Exception, f.close)
+        bio = BrokenBytesIO()
+        f = h5py.File(bio, 'w')
+        try:
+            self.assertRaises(Exception, f.create_dataset, 'test',
+                              data=list(range(12)))
+        finally:
+            # Un-break writing so we can close: errors while closing get messy.
+            bio.allow_write = True
+            f.close()
 
     def test_exception_close(self):
         fileobj = io.BytesIO()
