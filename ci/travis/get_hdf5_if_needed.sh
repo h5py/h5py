7f4ab1407a918271647310fb2f8b9d7f3fa572fc
Thomas Kluyver <thomas@kluyver.me.uk>
2020-09-17 16:39:57 +0100

    Download HDF5 with curl instead of wget

--- ci/travis/get_hdf5_if_needed.sh
+++ ci/travis/get_hdf5_if_needed.sh
@@ -12,8 +12,8 @@ else
         echo "using cached build"
     else
         pushd /tmp
-        #                             Remove trailing .*, to get e.g. '1.12' ↓
-        wget "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
+        #                                   Remove trailing .*, to get e.g. '1.12' ↓
+        curl -fsSLO "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
         tar -xzvf hdf5-$HDF5_VERSION.tar.gz
         pushd hdf5-$HDF5_VERSION
         chmod u+x autogen.sh
