e9e9e9a64a3da309040167102a59c5e8edcf8add
Thomas Kluyver <thomas@kluyver.me.uk>
2020-09-18 09:14:55 +0100

    Replace a couple more arr.tostring with tobytes
    These must have been overlooked before.

--- h5py/tests/test_h5d_direct_chunk.py
+++ h5py/tests/test_h5d_direct_chunk.py
@@ -72,7 +72,7 @@ class TestReadDirectChunk(TestCase):
                                                 compression_opts=9)
             # Write uncompressed data
             DISABLE_ALL_FILTERS = 0xFFFFFFFF
-            dataset.id.write_direct_chunk((0, 0, 0), frame.tostring(), filter_mask=DISABLE_ALL_FILTERS)
+            dataset.id.write_direct_chunk((0, 0, 0), frame.tobytes(), filter_mask=DISABLE_ALL_FILTERS)
 
         # FIXME: Here we have to close the file and load it back else
         #     a runtime error occurs:
@@ -83,7 +83,7 @@ class TestReadDirectChunk(TestCase):
 
         # At least 1 filter is supposed to be disabled
         self.assertNotEqual(filter_mask, 0)
-        self.assertEqual(compressed_frame, frame.tostring())
+        self.assertEqual(compressed_frame, frame.tobytes())
 
     def test_read_write_chunk(self):
 
