89fdef33947c33936c7aebd7bc8c0317854c0911
Thomas Kluyver <thomas@kluyver.me.uk>
2020-11-06 09:52:28 +0000

    bumped version to 3.1.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if '' not in sys.path:
 import setup_build, setup_configure
 
 
-VERSION = '3.0.0'
+VERSION = '3.1.0'
 
 # Minimum supported versions of Numpy & Cython depend on the Python version
 NUMPY_MIN_VERSIONS = [
