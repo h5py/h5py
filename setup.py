04adbe69c2397128ef394845c2823eb3a55081a6
Thomas Kluyver <thomas@kluyver.me.uk>
2021-01-06 11:56:25 +0000

    Those don't need to be f strings

--- setup.py
+++ setup.py
@@ -46,9 +46,9 @@ RUN_REQUIRES = ["cached-property; python_version<'3.8'"] + [
 # versions.
 SETUP_REQUIRES = [
     'pkgconfig',
-    f"Cython >=0.29; python_version<'3.8'",
-    f"Cython >=0.29.14; python_version=='3.8'",
-    f"Cython >=0.29.15; python_version>='3.9'",
+    "Cython >=0.29; python_version<'3.8'",
+    "Cython >=0.29.14; python_version=='3.8'",
+    "Cython >=0.29.15; python_version>='3.9'",
 ] + [
     f"numpy =={np_min}; python_version{py_condition}"
     for np_min, py_condition in NUMPY_MIN_VERSIONS
