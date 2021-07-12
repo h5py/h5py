45597d02cdd622763487359a0f5664b0010cde99
Tanveen Bharaj <tbbharaj@amazon.com>
2021-07-12 17:40:27 -0500

    Test aarch64 wheels upload to https://anaconda.org/test-upload

--- ci/get_hdf5_arm64.sh
+++ ci/get_hdf5_arm64.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-set -e -u -x
-
-# Installing hdf5 dependency
 echo "Installing zlib with yum"
 export HDF5_VERSION=1.12.0
 export HDF5_DIR="/usr/local"
@@ -27,19 +23,3 @@ rm -r hdf5-$HDF5_VERSION
 rm hdf5-$HDF5_VERSION.tar.gz
 
 yum -y erase zlib-devel
-
-# Building wheels
-cd /src/
-
-# Create binary wheels
-python=`ls /opt/python/cp*/bin/pip |grep "cp3[789]"`
-for bindir in $python; do
-    "$bindir" wheel /src/ -w wheelhouse/
-done
-
-ls -lrt wheelhouse/
-
-# Normalize resulting binaries to a common format
-for whl in wheelhouse/h5py-*.whl; do
-    auditwheel repair "$whl" -w wheelhouse/
-done
