0b8cbe08524db79599e75a8ef2cd4104bed446b9
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-08-25 12:53:21 +0200

    Add get|set_file_locking to PropFAID and tests

--- h5py/tests/test_h5p.py
+++ h5py/tests/test_h5p.py
@@ -91,6 +91,18 @@ class TestFA(TestCase):
         self.assertEqual((threshold, alignment),
                          falist.get_alignment())
 
+    @ut.skipIf(version.hdf5_version_tuple < (1, 12, 1),
+               'Requires HDF5 1.12.1 or later')
+    def test_set_file_locking(self):
+        '''test get/set file locking'''
+        falist = h5p.create(h5p.FILE_ACCESS)
+        use_file_locking = False
+        ignore_when_disabled = False
+
+        falist.set_file_locking(use_file_locking, ignore_when_disabled)
+        self.assertEqual((use_file_locking, ignore_when_disabled),
+                         falist.get_file_locking())
+
 
 class TestPL(TestCase):
     def test_obj_track_times(self):
