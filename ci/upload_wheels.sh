2903031dde5217834dee7b90369e91c166e5ce6f
Tanveen Bharaj <tbbharaj@amazon.com>
2021-10-27 22:01:10 -0500

    Fix wheel tagging

--- ci/upload_wheels.sh
+++ ci/upload_wheels.sh
@@ -3,9 +3,9 @@ pip install git+https://github.com/Anaconda-Server/anaconda-client;
 
 # rename wheels
 # appending timestamp to wheels package name
-# e.g. h5py-3.3.0-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl to h5py-3.3.0-cp37-cp37m-20211004151033-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
+# e.g. h5py-3.3.0-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl to h5py-3.3.0-20211004151033-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
 for whl in ${TRAVIS_BUILD_DIR}/wheelhouse/h5py-*.whl; do 
-	newname=$(echo "$whl" | sed "s/\(-[cp3(789)]*[m]*-\)\(manylinux*\)/\1$(date '+%Y%m%d%H%M%S')-\2/")
+	newname=$(echo "$whl" | sed "s/\(h5py-[0-9][0-9]*[.[0-9]*]*-\)\(cp*\)/\1$(date '+%Y%m%d%H%M%S')-\2/")
    if [ "$newname" != "$whl" ]; then
        mv $whl $newname
    fi
