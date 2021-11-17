4320f2ea786d1c5087ab41c5be56b51b2683bcc0
Thomas Kluyver <thomas@kluyver.me.uk>
2021-11-17 11:59:23 +0000

    Refine how aarch64 wheels are named for upload to anaconda.org

--- ci/upload_wheels.sh
+++ ci/upload_wheels.sh
@@ -1,17 +1,27 @@
 ANACONDA_ORG="scipy-wheels-nightly";
 pip install git+https://github.com/Anaconda-Server/anaconda-client;
 
-# rename wheels
-# appending timestamp to wheels package name
-# e.g. h5py-3.3.0-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl to h5py-3.3.0-20211004151033-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
-for whl in ${TRAVIS_BUILD_DIR}/wheelhouse/h5py-*.whl; do
-	newname=$(echo "$whl" | sed "s/\(h5py-[0-9][0-9]*[.[0-9]*]*-\)\(cp*\)/\1$(date '+%Y%m%d%H%M%S')-\2/")
-   if [ "$newname" != "$whl" ]; then
-       mv $whl $newname
-   fi
-done
+if [[ "$TRAVIS_EVENT_TYPE" != "cron" && -z "$TRAVIS_TAG" ]] ; then
+  echo "Not uploading wheels (build not for cron or git tag)"
+  exit 0
+fi
+
+# rename wheels if not on a tag
+# inserting short commit hash as build tag in wheel filename
+# e.g. h5py-3.3.0-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl to
+#   h5py-3.3.0-0e2d161a0-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
+if [[ -z "$TRAVIS_TAG" ]] ; then
+  # build tag has to start with a decimal digit, so prefix 0 on commit hash
+  build_tag="0$(git rev-parse --short=8 HEAD)"
+  for whl in "${TRAVIS_BUILD_DIR}"/wheelhouse/h5py-*.whl; do
+    newname=$(echo "$whl" | sed "s/\(h5py-[0-9][0-9]*[.[0-9]*]*-\)\(cp*\)/\1${build_tag}-\2/")
+     if [ "$newname" != "$whl" ]; then
+         mv "$whl" "$newname"
+     fi
+  done
+fi
 
 # upload wheels
 if [[ -n "${ANACONDA_ORG_UPLOAD_TOKEN}" ]] ; then
-   anaconda -t ${ANACONDA_ORG_UPLOAD_TOKEN} upload --force -u ${ANACONDA_ORG} ${TRAVIS_BUILD_DIR}/wheelhouse/h5py-*.whl;
+   anaconda -t ${ANACONDA_ORG_UPLOAD_TOKEN} upload -u ${ANACONDA_ORG} "${TRAVIS_BUILD_DIR}"/wheelhouse/h5py-*.whl;
 fi;
