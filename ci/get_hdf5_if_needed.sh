3086fa792c6e4d68273bc3caffa1dc81ced9b390
Matthew Einhorn <matt@einhorn.dev>
2022-03-14 17:21:47 -0400

    Use configure for older hdf5 versions.

--- ci/get_hdf5_if_needed.sh
+++ ci/get_hdf5_if_needed.sh
@@ -6,21 +6,26 @@ if [ -z ${HDF5_DIR+x} ]; then
     echo "Using OS HDF5"
 else
     echo "Using downloaded HDF5"
+
+    extra_arch_flags=()
+    EXTRA_CMAKE_MPI_FLAGS=""
+    EXTRA_MPI_FLAGS=''
     if [ -z ${HDF5_MPI+x} ]; then
         echo "Building serial"
-        EXTRA_MPI_FLAGS=''
     else
         echo "Building with MPI"
-        EXTRA_MPI_FLAGS="-DHDF5_ENABLE_PARALLEL:bool=on"
+        EXTRA_CMAKE_MPI_FLAGS="-DHDF5_ENABLE_PARALLEL:bool=on"
+        EXTRA_MPI_FLAGS="--enable-parallel"
     fi
 
     if [[ "$OSTYPE" == "darwin"* ]]; then
         lib_name=libhdf5.dylib
+        NPROC=$(sysctl -n hw.ncpu)
     else
         lib_name=libhdf5.so
+        NPROC=$(nproc)
     fi
 
-    extra_arch_flags=()
     if [ -f $HDF5_DIR/lib/$lib_name ]; then
         echo "using cached build"
     else
@@ -56,10 +61,15 @@ else
         curl -fsSLO "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
         tar -xzvf hdf5-$HDF5_VERSION.tar.gz
         pushd hdf5-$HDF5_VERSION
-        mkdir build
-        cd build
-        cmake -DCMAKE_INSTALL_PREFIX="$HDF5_DIR" -DENABLE_SHARED:bool=on $EXTRA_MPI_FLAGS "${extra_arch_flags[@]}" ../
-        make -j $(nproc)
+
+        if [[ $MAJOR_V -gt 1 || $MINOR_V -ge 13 ]]; then
+            mkdir build
+            cd build
+            cmake -DCMAKE_INSTALL_PREFIX="$HDF5_DIR" -DENABLE_SHARED:bool=on $EXTRA_CMAKE_MPI_FLAGS "${extra_arch_flags[@]}" ../
+        else
+            ./configure --prefix "$HDF5_DIR" $EXTRA_MPI_FLAGS --enable-shared
+        fi
+        make -j "$NPROC"
         make install
         popd
         popd
