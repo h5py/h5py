7f9ee8d8c1e939f091d0f65841bb60fbf74eb98d
Daniel Hay Guest <dhguest@gmail.com>
2022-04-01 12:01:07 +0200

    Update h5py/_selector.pyx: use kind == 'b' as type check
    Co-authored-by: Thomas Kluyver <takowl@gmail.com>

--- h5py/_selector.pyx
+++ h5py/_selector.pyx
@@ -180,7 +180,7 @@ cdef class Selector:
                 if a.ndim != 1:
                     raise TypeError("Only 1D arrays allowed for fancy indexing")
                 # Fix for issue #1847, not sure this is optimal but it works
-                if np.issubdtype(a.dtype, bool):
+                if a.dtype.kind == 'b':
                     if a.size != l:
                         raise TypeError("Boolean index array must match dataset size")
                     a = a.nonzero()[0]
