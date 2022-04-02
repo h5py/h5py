c5e17907f0f2ab981853b4dc33938519ece27e5e
Matthew Einhorn <matt@einhorn.dev>
2022-04-01 22:34:43 -0400

    Fix release path.

--- ci/configure_hdf5_mac.sh
+++ ci/configure_hdf5_mac.sh
@@ -12,7 +12,7 @@ function build_zlib() {
     ZLIB_VERSION="1.2.11"
 
     pushd /tmp
-    curl -sLO https://zlib.net/zlib-$ZLIB_VERSION.tar.gz
+    curl -sLO https://zlib.net/fossils/zlib-$ZLIB_VERSION.tar.gz
     tar xzf zlib-$ZLIB_VERSION.tar.gz
     cd zlib-$ZLIB_VERSION
     ./configure --prefix="$HDF5_DIR"
