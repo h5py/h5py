f8e28efbb7cd7c64443821b98ca1d10dffd40344
Dan Guest <dguest@cern.ch>
2022-04-04 21:15:11 +0200

    Move bool index test to test_dataset_getitem

--- h5py/tests/test_dataset.py
+++ h5py/tests/test_dataset.py
@@ -1810,22 +1810,3 @@ class TestCommutative(BaseDataset):
         val = float(0.)
         assert (val == dset) == (dset == val)
         assert (val != dset) == (dset != val)
-
-
-class TestBoolIndex(BaseDataset):
-    """
-    Tests for indexing with Boolean arrays
-    """
-    def setUp(self):
-        super().setUp()
-        self.arr = np.arange(9).reshape(3,-1)
-        self.dset = self.f.create_dataset('x', data=self.arr)
-
-    def test_select_first_axis(self):
-        sel = np.s_[[False, True, False],:]
-        self.assertNumpyBehavior(self.dset, self.arr, sel)
-
-    def test_wrong_size(self):
-        sel = np.s_[[False, True, False, False],:]
-        with self.assertRaises(TypeError):
-            self.dset[sel]
--- h5py/tests/test_dataset_getitem.py
+++ h5py/tests/test_dataset_getitem.py
@@ -586,3 +586,22 @@ class TestVeryLargeArray(TestCase):
     @ut.skipIf(sys.maxsize < 2**31, 'Maximum integer size >= 2**31 required')
     def test_size(self):
         self.assertEqual(self.dset.size, 2**31)
+
+
+class TestBoolIndex(TestCase):
+    """
+    Tests for indexing with Boolean arrays
+    """
+    def setUp(self):
+        super().setUp()
+        self.arr = np.arange(9).reshape(3,-1)
+        self.dset = self.f.create_dataset('x', data=self.arr)
+
+    def test_select_first_axis(self):
+        sel = np.s_[[False, True, False],:]
+        self.assertNumpyBehavior(self.dset, self.arr, sel)
+
+    def test_wrong_size(self):
+        sel = np.s_[[False, True, False, False],:]
+        with self.assertRaises(TypeError):
+            self.dset[sel]
