444e9bbd256c720fd0f91dc817fac88355349cfe
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-02-10 16:13:09 +0100

    add some tests for failing cases

--- h5py/tests/test_h5d_direct_chunk.py
+++ h5py/tests/test_h5d_direct_chunk.py
@@ -170,3 +170,31 @@ class TestReadDirectChunkToBuffer:
         assert filter_mask == chunk_info.filter_mask
         assert nbytes == chunk_info.size
         assert out == dataset.id.read_direct_chunk(chunk_info.chunk_offset)[1]
+
+    def test_fail_buffer_too_small(self, writable_file):
+        ref_data = numpy.arange(16).reshape(4, 4)
+        dataset = writable_file.create_dataset(
+            "uncompressed", data=ref_data, chunks=ref_data.shape)
+
+        out = bytearray(ref_data.nbytes // 2)
+        with pytest.raises(ValueError):
+            dataset.id.read_direct_chunk_tobuffer((0, 0), out)
+
+    def test_fail_buffer_readonly(self, writable_file):
+        ref_data = numpy.arange(16).reshape(4, 4)
+        dataset = writable_file.create_dataset(
+            "uncompressed", data=ref_data, chunks=ref_data.shape)
+
+        out = bytes(ref_data.nbytes // 2)
+        with pytest.raises(BufferError):
+            dataset.id.read_direct_chunk_tobuffer((0, 0), out)
+
+    def test_fail_buffer_not_contiguous(self, writable_file):
+        ref_data = numpy.arange(16).reshape(4, 4)
+        dataset = writable_file.create_dataset(
+            "uncompressed", data=ref_data, chunks=ref_data.shape)
+
+        array = numpy.empty(ref_data.shape + (2,), dtype=ref_data.dtype)
+        out = array[:, :, ::2]  # Array is not contiguous
+        with pytest.raises(ValueError):
+            dataset.id.read_direct_chunk_tobuffer((0, 0), out)
