075c61a31558455f00d433d48140039b731d0a57
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-02-10 15:44:50 +0100

    add tests for DatasetID.read_direct_chunk_tobuffer

--- h5py/tests/test_h5d_direct_chunk.py
+++ h5py/tests/test_h5d_direct_chunk.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy
 import numpy.testing
+import pytest
 
 from .common import ut, TestCase
 
@@ -113,3 +114,59 @@ class TestReadDirectChunk(TestCase):
         with h5py.File(filename, "r") as filehandle:
             dataset = filehandle["created"][...]
             numpy.testing.assert_array_equal(dataset, frame)
+
+
+@pytest.mark.skipif(
+    h5py.version.hdf5_version_tuple < (1, 10, 2),
+    reason="Direct chunk read requires HDF5 >= 1.10.2"
+)
+class TestReadDirectChunkToBuffer:
+
+    def test_uncompressed_data(self, writable_file):
+        ref_data = numpy.arange(16).reshape(4, 4)
+        dataset = writable_file.create_dataset(
+            "uncompressed", data=ref_data, chunks=ref_data.shape)
+
+        # Read to numpy array
+        out_ndarray = numpy.zeros_like(ref_data)
+        filter_mask, nbytes = dataset.id.read_direct_chunk_tobuffer((0, 0), out_ndarray)
+
+        assert numpy.array_equal(out_ndarray, ref_data)
+        assert filter_mask == 0
+        assert nbytes == ref_data.nbytes
+
+        # Read to bytearray
+        out_bytearray = bytearray(ref_data.nbytes)
+        filter_mask, nbytes = dataset.id.read_direct_chunk_tobuffer((0, 0), out_bytearray)
+
+        assert numpy.array_equal(
+            numpy.frombuffer(out_bytearray, dtype=ref_data.dtype).reshape(ref_data.shape),
+            ref_data,
+        )
+        assert filter_mask == 0
+        assert nbytes == ref_data.nbytes
+
+    @pytest.mark.skipif(
+        h5py.version.hdf5_version_tuple < (1, 10, 5),
+        reason="chunk info requires HDF5 >= 1.10.5",
+    )
+    def test_compressed_data(self, writable_file):
+        ref_data = numpy.arange(16).reshape(4, 4)
+        dataset = writable_file.create_dataset(
+            "gzip",
+            data=ref_data,
+            chunks=ref_data.shape,
+            compression="gzip",
+            compression_opts=9,
+        )
+        chunk_info = dataset.id.get_chunk_info(0)
+
+        # read to bytearray
+        out = bytearray(chunk_info.size)
+        filter_mask, nbytes = dataset.id.read_direct_chunk_tobuffer(
+            chunk_info.chunk_offset,
+            out,
+        )
+        assert filter_mask == chunk_info.filter_mask
+        assert nbytes == chunk_info.size
+        assert out == dataset.id.read_direct_chunk(chunk_info.chunk_offset)[1]
