f80ddc0699a48f2e87830390a300694ca623e71b
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2023-09-12 12:38:46 -0400

    Improve ros3 test coverage

--- h5py/tests/test_ros3.py
+++ h5py/tests/test_ros3.py
@@ -12,7 +12,9 @@
 """
 
 import h5py
+from h5py._hl.files import make_fapl
 import pytest
+
 
 pytestmark = [
     pytest.mark.skipif(
@@ -55,8 +57,9 @@ def test_ros3_s3uri():
                     reason='AWS S3 access token support in HDF5 >= 1.14.2')
 def test_ros3_temp_token():
     """Set and get S3 access token"""
-    fapl = h5py.h5p.create(h5py.h5p.FILE_ACCESS)
-    fapl.set_fapl_ros3()
     token = b'#0123FakeToken4567/8/9'
-    fapl.set_fapl_ros3_token(token)
+    fapl = make_fapl('ros3', libver=None, rdcc_nslots=None, rdcc_nbytes=None,
+                     rdcc_w0=None, locking=None, page_buf_size=None, min_meta_keep=None,
+                     min_raw_keep=None, alignment_threshold=1, alignment_interval=1,
+                     meta_block_size=None, session_token=token)
     assert token, fapl.get_fapl_ros3_token()
