09242ae711534b8601dd1216de002faa16a709c3
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2023-11-16 10:18:22 +0100

    Reopen Blosc2 opt slicing unit test file in read-only mode
    To ensure that no HDF5/h5py caching takes place, so that data must be read
    either by the optimized path or the filter pipeline.

--- h5py/tests/test_slicing_blosc2.py
+++ h5py/tests/test_slicing_blosc2.py
@@ -31,10 +31,16 @@ from h5py import File
 
 class StoreArrayMixin:
     # Requires: self.f (read/write), self.arr, self.chunks
-    # Provides: self.dset
+    # Provides: self.f (read-only), self.dset
     def setUp(self):
         comp = h5p.Blosc2(cname='lz4', clevel=5, filters=h5p.Blosc2.SHUFFLE)
         self.f.create_dataset('x', data=self.arr, chunks=self.chunks, **comp)
+
+        # Reopen the test file read-only to ensure
+        # that no HDF5/h5py caching takes place.
+        fn = self.f.filename
+        self.f.close()
+        self.f = File(fn, 'r')
         self.dset = self.f['x']
 
 
