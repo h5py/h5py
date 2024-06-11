c0c4ca0fa92e00cf1fd68035f11928fe6ff1c311
Eric Larson <larson.eric.d@gmail.com>
2024-06-11 19:33:05 -0400

    FIX: Speed up aarch64

--- ci/cibw_test_command.sh
+++ ci/cibw_test_command.sh
@@ -12,11 +12,15 @@ WHEEL_PATH=$2
 echo "WHEEL_PATH=$WHEEL_PATH"
 
 export PYVER=$(python -c "import sys; print(''.join(map(str, sys.version_info[:2])))")
-export TOXENV="py$PYVER-test-deps,py$PYVER-test-mindeps,py$PYVER-test-deps-pre"
+if [[ "$TOX_TEST_LIMITED" == "1" ]]; then  # e.g., aarch64
+    ENVLIST="py$PYVER-test-deps"
+else
+    ENVLIST="py$PYVER-test-deps,py$PYVER-test-mindeps,py$PYVER-test-deps-pre"
+fi
 export H5PY_TEST_CHECK_FILTERS=1
-echo "TOXENV=$TOXENV"
+echo "ENVLIST=$ENVLIST"
 cd $PROJECT_PATH
-tox --installpkg $WHEEL_PATH
+tox --installpkg $WHEEL_PATH -e $ENVLIST
 if [[ "$GITHUB_ACTION" != "" ]]; then
     echo "Uploading coverage using python=$(which python)"
     python ./ci/upload_coverage.py --codecov-token 813fb6da-087d-4b36-a185-5a530cab3455
