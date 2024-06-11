8af2c85950075711030cca5e6000db1c546bd853
Eric Larson <larson.eric.d@gmail.com>
2024-06-11 16:07:31 -0400

    FIX: Fix macOS

--- ci/cibw_before_all_macos.sh
+++ ci/cibw_before_all_macos.sh
@@ -10,12 +10,18 @@ PROJECT_PATH="$1"
 ARCH=$(uname -m)
 export HDF5_VERSION="1.12.2"
 export HDF5_DIR="$PROJECT_PATH/cache/hdf5/$HDF5_VERSION-$ARCH"
-export MACOSX_DEPLOYMENT_TARGET="10.9"
+# When compiling HDF5, we should use the minimum across all Python versions for a given
+# arch, for versions see for example a more updated version of the following:
+# https://github.com/pypa/cibuildwheel/blob/89a5cfe2721c179f4368a2790669e697759b6644/cibuildwheel/macos.py#L296-L310
+if [[ "$ARCH" == "arm64" ]]; then
+    export MACOSX_DEPLOYMENT_TARGET="11.0"
+else
+    export MACOSX_DEPLOYMENT_TARGET="10.9"
+fi
 source $PROJECT_PATH/ci/get_hdf5_if_needed.sh
 
 if [[ "$GITHUB_ENV" != "" ]]; then
     echo "HDF5_DIR=$HDF5_DIR" >> $GITHUB_ENV
-    echo "MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET" >> $GITHUB_ENV
     echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $GITHUB_ENV
     echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >> $GITHUB_ENV
 fi
