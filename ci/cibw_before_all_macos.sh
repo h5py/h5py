33b43aa2963d58c795e74c4eba373a7dabbd65d1
Eric Larson <larson.eric.d@gmail.com>
2024-06-11 14:20:27 -0400

    FIX: More

---
+++ ci/cibw_before_all_macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [[ "$1" == "" ]] ; then
+    echo "Usage: $0 <PROJECT_PATH>"
+    exit 1
+fi
+PROJECT_PATH="$1"
+export HDF5_VERSION="1.12.2"
+export HDF5_DIR="$PROJECT_PATH/cache/hdf5/$HDF5_VERSION"
+source $PROJECT_PATH/ci/get_hdf5_if_needed.sh
+if [[ "$GITHUB_ENV" != "" ]]; then
+    echo "HDF5_DIR=$HDF5_DIR" >> $GITHUB_ENV
+    echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $GITHUB_ENV
+    echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >> $GITHUB_ENV
+fi
---
+++ ci/cibw_before_all_windows.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [[ "$1" == "" ]] ; then
+    echo "Usage: $0 <PROJECT_PATH>"
+    exit 1
+fi
+PROJECT_PATH="$1"
+
+# nuget
+nuget install zlib-msvc-x64 -ExcludeVersion -OutputDirectory "$PROJECT_PATH"
+export PATH="$PATH:$PROJECT_PATH\zlib-msvc-x64\build\native\bin_release"
+export CL="/I$PROJECT_PATH\zlib-msvc-x64\build\native\include"
+export LINK="/LIBPATH:$PROJECT_PATH\zlib-msvc-x64\build\native\lib_release"
+export ZLIB_ROOT="$PROJECT_PATH\zlib-msvc-x64\build\native"
+
+# HDF5
+export HDF5_VERSION="1.14.2"
+export HDF5_VSVERSION="16-64"
+export HDF5_DIR="$PROJECT_PATH/cache/hdf5/$HDF5_VERSION"
+python $PROJECT_PATH/ci/get_hdf5_win.py
+
+if [[ "$GITHUB_ENV" != "" ]] ; then
+    echo "PATH=$PATH" >> $GITHUB_ENV
+    echo "CL=$CL" >> $GITHUB_ENV
+    echo "LINK=$LINK" >> $GITHUB_ENV
+    echo "ZLIB_ROOT=$ZLIB_ROOT" >> $GITHUB_ENV
+    echo "HDF5_DIR=$HDF5_DIR" >> $GITHUB_ENV
+fi
