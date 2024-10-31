b6bf3e049d5f399fe94fc9fbc2d10227b3d180f5
Thomas Kluyver <takowl@gmail.com>
2024-10-31 15:29:40 +0000

    Simplify wording in docstring

--- h5py/_hl/group.py
+++ h5py/_hl/group.py
@@ -90,7 +90,7 @@ class Group(HLObject, MutableMappingHDF5):
         maxshape
             (Tuple or int) Make the dataset resizable up to this shape. Use None for
             axes within the tuple you want to be unlimited. Integers can be used for 1D shape.
-            For 1D datsets with quasi-unlimited maxshape, a shape tuple of length 1 must be
+            For 1D datsets with unlimited maxshape, a shape tuple of length 1 must be
             provided, ``(None,)``. Passing ``None`` sets ``maxshape` to `shape`, making the
             dataset un-resizable, which is the default.
         compression
