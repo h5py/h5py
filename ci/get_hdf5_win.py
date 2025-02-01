4853686da38d5659c1e7e8318c48e75cbdea72d0
Thomas Kluyver <thomas.kluyver@xfel.eu>
2025-02-01 10:17:54 +0000

    Test building HDF5 on Windows without CMakeFilters.cmake patch

--- ci/get_hdf5_win.py
+++ ci/get_hdf5_win.py
@@ -95,7 +95,6 @@ def build_hdf5(version, hdf5_file, install_path, cmake_generator, use_prefix,
 
             with ZipFile(hdf5_file) as z:
                 z.extractall(hdf5_extract_path)
-            replace_CMakeFilters_cmake(hdf5_extract_path, dl_zip)
 
             old_dir = getcwd()
 
