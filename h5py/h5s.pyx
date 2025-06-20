0e9efcdfa7c4ecbcdee9bf65f325066723df29ea
iguinn <guinnis@ornl.gov>
2025-06-20 08:03:24 -0700

    Fixed bug in h5s.select_copy

--- h5py/h5s.pyx
+++ h5py/h5s.pyx
@@ -415,7 +415,7 @@ cdef class SpaceID(ObjectID):
 
         Copies a selection from one dataspace to another.
         """
-        H5Sselect_copy(self.id, src_id)
+        H5Sselect_copy(self.id, src_id.id)
 
 
     @with_phil
