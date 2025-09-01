b87418d9aadd9a41216db191de4280a67834cccc
crusaderky <crusaderky@gmail.com>
2025-09-01 10:25:45 +0100

    DNM

--- DNM_build_test.sh
+++ DNM_build_test.sh
@@ -3,6 +3,7 @@ rm -rf dist
 python -m build
 pip install dist/*.whl --force-reinstall --no-deps
 mkdir -p /tmp/h5py
+cp pytest.ini /tmp/h5py/
 cd /tmp/h5py
 # export PYTHON_GIL=0
-python -m pytest --pyargs h5py -v --parallel-threads=32
+python -m pytest --pyargs h5py -v --durations 10 --parallel-threads=32
