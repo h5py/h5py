1a34e6b4da1b0fbf04d8054a26e9e3bebbc2c5aa
crusaderky <crusaderky@gmail.com>
2025-09-23 18:06:31 +0100

    DNM Enable cp313t and cp314t wheels

--- ci/triage_build.sh
+++ ci/triage_build.sh
@@ -21,7 +21,7 @@ KIND="$RUNNER_OS $ARCH"
 # If it's a scheduled build or [pip-pre] in commit message, use pip-pre
 if [[ "$GITHUB_EVENT_NAME" == "schedule" ]] || [[ "$MSG" = *'[pip-pre]'* ]]; then
     echo "Using NumPy pip-pre wheel; setting CIBW_BEFORE_BUILD and CIBW_BUILD_FRONTEND"
-    echo "CIBW_BEFORE_BUILD=pip install --pre --only-binary numpy --extra-index-url https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \"numpy>=2.0.0.dev0\" \"Cython>=0.29.31,<4\" pkgconfig \"setuptools>=77\"" | tee -a $GITHUB_ENV
+    echo "CIBW_BEFORE_BUILD=pip install --pre --only-binary numpy --extra-index-url https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \"numpy>=2.0.0.dev0\" \"Cython>=0.29.31,<4\" packaging>=23.0 pkgconfig \"setuptools>=77\"" | tee -a $GITHUB_ENV
     echo "CIBW_BUILD_FRONTEND=pip; args: --no-build-isolation" | tee -a $GITHUB_ENV
 fi
 
