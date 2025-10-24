2ebff09e132436f576c8f9a0177f938f9e121788
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2025-10-24 13:30:07 +0200

    Update h5py/_hl/dataset.py
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- h5py/_hl/dataset.py
+++ h5py/_hl/dataset.py
@@ -1061,11 +1061,11 @@ class Dataset(HLObject):
         # Perform the dataspace selection
         selection = sel.select(self.shape, args, dataset=self)
         # return early on zero-size selection
-        # but not if dataset shape and selection shape are equal
+        # but only if dataset shape and selection shape differ
         # this prevents returning here for zero size datasets
         # where proper errors should be raised later
         # see https://github.com/h5py/h5py/issues/2549
-        if selection.nselect == 0 and not (self.shape == selection.shape):
+        if selection.nselect == 0 and self.shape != selection.shape:
             return
 
         # Broadcast scalars if necessary.
