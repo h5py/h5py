a36ed87fdb23e1518364161308095f38b1a26492
Clément Robert <cr52@protonmail.com>
2026-02-11 19:54:38 +0100

    TST: parametrize test cases

--- h5py/tests/test_native_complex.py
+++ h5py/tests/test_native_complex.py
@@ -32,44 +32,56 @@ pytestmark = [
     ),
 ]
 
+REQUIRE_NUMPY_LONG_COMPLEX = pytest.mark.skipif(
+    not hasattr(np, "complex256"),
+    reason="long complex type is not available in numpy",
+)
 
-def test_hdf5_dtype():
+NATIVE_BYTE_ORDER = h5t.ORDER_BE if sys.byteorder == "big" else h5t.ORDER_LE
+
+
+@pytest.mark.parametrize(
+    "h5type_str, size, order",
+    [
+        pytest.param("COMPLEX_IEEE_F16LE", 4, h5t.ORDER_LE),
+        pytest.param("COMPLEX_IEEE_F16BE", 4, h5t.ORDER_BE, id="F16BE"),
+        pytest.param("COMPLEX_IEEE_F32LE", 8, h5t.ORDER_LE, id="F32LE"),
+        pytest.param("COMPLEX_IEEE_F32BE", 8, h5t.ORDER_BE, id="F32BE"),
+        pytest.param("COMPLEX_IEEE_F64LE", 16, h5t.ORDER_LE, id="F64LE"),
+        pytest.param("COMPLEX_IEEE_F64BE", 16, h5t.ORDER_BE, id="F64BE"),
+        pytest.param("NATIVE_FLOAT_COMPLEX", 8, NATIVE_BYTE_ORDER, id="float-native"),
+        pytest.param("NATIVE_DOUBLE_COMPLEX", 16, NATIVE_BYTE_ORDER, id="double-native"),
+        pytest.param(
+            "NATIVE_LDOUBLE_COMPLEX", 32, NATIVE_BYTE_ORDER,
+            id="long-native",
+            marks=REQUIRE_NUMPY_LONG_COMPLEX,
+        )
+    ]
+)
+def test_hdf5_dtype(h5type_str, size, order):
     """Low-level checks of HDF5 native complex number datatypes"""
-    sys_bo = h5t.ORDER_BE if sys.byteorder == "big" else h5t.ORDER_LE
-    cases = [
-        (h5t.COMPLEX_IEEE_F16LE, 4, h5t.ORDER_LE),
-        (h5t.COMPLEX_IEEE_F16BE, 4, h5t.ORDER_BE),
-        (h5t.COMPLEX_IEEE_F32LE, 8, h5t.ORDER_LE),
-        (h5t.COMPLEX_IEEE_F32BE, 8, h5t.ORDER_BE),
-        (h5t.COMPLEX_IEEE_F64LE, 16, h5t.ORDER_LE),
-        (h5t.COMPLEX_IEEE_F64BE, 16, h5t.ORDER_BE),
-        (h5t.NATIVE_FLOAT_COMPLEX, 8, sys_bo),
-        (h5t.NATIVE_DOUBLE_COMPLEX, 16, sys_bo),
-    ]
-    if hasattr(np, "complex256"):
-        cases.append((h5t.NATIVE_LDOUBLE_COMPLEX, 32, sys_bo))
-    else:
-        cases.append((h5t.NATIVE_LDOUBLE_COMPLEX, 16, sys_bo))
-    for h5type, size, order in cases:
-        assert isinstance(h5type, h5t.TypeComplexID)
-        assert h5type.get_size() == size
-        assert h5type.get_order() == order
+    h5type = getattr(h5t, h5type_str)
+    assert isinstance(h5type, h5t.TypeComplexID)
+    assert h5type.get_size() == size
+    assert h5type.get_order() == order
 
+H5_VS_NUMPY_DTYPES = [
+    pytest.param("COMPLEX_IEEE_F32LE", "<c8", id="<c8"),
+    pytest.param("COMPLEX_IEEE_F32BE", ">c8", id=">c8"),
+    pytest.param("COMPLEX_IEEE_F64LE", "<c16", id="<c16"),
+    pytest.param("COMPLEX_IEEE_F64BE", ">c16", id=">c16"),
+    pytest.param("NATIVE_FLOAT_COMPLEX", "=c8", id="=c8"),
+    pytest.param("NATIVE_DOUBLE_COMPLEX", "=c16", id="=c16"),
+    pytest.param(
+        "NATIVE_LDOUBLE_COMPLEX", "=c32", id="=c32",
+        marks=REQUIRE_NUMPY_LONG_COMPLEX,
+    ),
+]
 
-def test_cmplx_type_trnslt():
+@pytest.mark.parametrize("h5type_str, dt", H5_VS_NUMPY_DTYPES)
+def test_cmplx_type_trnslt(h5type_str, dt):
     """Translate native HDF5 complex number datatype to NumPy dtype"""
-    cases = [
-        (h5t.COMPLEX_IEEE_F32LE, np.dtype("<c8")),
-        (h5t.COMPLEX_IEEE_F32BE, np.dtype(">c8")),
-        (h5t.COMPLEX_IEEE_F64LE, np.dtype("<c16")),
-        (h5t.COMPLEX_IEEE_F64BE, np.dtype(">c16")),
-        (h5t.NATIVE_FLOAT_COMPLEX, np.dtype("=c8")),
-        (h5t.NATIVE_DOUBLE_COMPLEX, np.dtype("=c16")),
-    ]
-    if hasattr(np, "complex256"):
-        cases.append((h5t.NATIVE_LDOUBLE_COMPLEX, np.dtype("=c32")))
-    for h5type, dt in cases:
-        assert h5type.dtype == dt
+    assert getattr(h5t, h5type_str).dtype == np.dtype(dt)
 
 
 def check_compound_complex_datatype(datatype, np_dtype):
@@ -86,56 +98,49 @@ def check_compound_complex_datatype(datatype, np_dtype):
        + datatype.get_member_type(1).get_size()
     ) == np_dtype.itemsize
 
-    if np_dtype.byteorder == ">" or (
-            sys.byteorder == "big" and np_dtype.byteorder == "="
-    ):
-        assert datatype.get_member_type(0).get_order() == h5t.ORDER_BE
-        assert datatype.get_member_type(1).get_order() == h5t.ORDER_BE
-    elif np_dtype.byteorder == "<" or (
-            sys.byteorder == "little" and np_dtype.byteorder == "="
-    ):
-        assert datatype.get_member_type(0).get_order() == h5t.ORDER_LE
-        assert datatype.get_member_type(1).get_order() == h5t.ORDER_LE
+    match (np_dtype.byteorder, sys.byteorder):
+        case (">", _) | ("=", "big"):
+            expected = h5t.ORDER_BE
+        case ("<", _) | ("=", "little"):
+            expected = h5t.ORDER_LE
+        case _ as _unreachable:
+            raise AssertionError
+
+    assert datatype.get_member_type(0).get_order() == expected
+    assert datatype.get_member_type(1).get_order() == expected
 
 
-def test_default_create(writable_file):
+@pytest.mark.parametrize("dt", ["<c8", ">c8", "<c16", ">c16"])
+def test_default_create(writable_file, dt):
     """Test default translation when creating datasets & attributes"""
-    for dt in ("<c8", ">c8", "<c16", ">c16"):
-        complex_array = (np.random.rand(100) + 1j * np.random.rand(100)).astype(dt)
-        ds = writable_file.create_dataset(dt, data=complex_array)
-        c = np.array(1.9 + 1j * 6.7, dtype=dt)
-        ds.attrs["c"] = c
-        check_compound_complex_datatype(ds.id.get_type(), np.dtype(dt))
-        check_compound_complex_datatype(ds.attrs.get_id("c").get_type(), np.dtype(dt))
-        np.testing.assert_array_equal(ds[...], complex_array)
-        np.testing.assert_array_equal(ds.attrs["c"], c)
+    complex_array = (np.random.rand(100) + 1j * np.random.rand(100)).astype(dt)
+    ds = writable_file.create_dataset(dt, data=complex_array)
+    c = np.array(1.9 + 1j * 6.7, dtype=dt)
+    ds.attrs["c"] = c
+    check_compound_complex_datatype(ds.id.get_type(), np.dtype(dt))
+    check_compound_complex_datatype(ds.attrs.get_id("c").get_type(), np.dtype(dt))
+    np.testing.assert_array_equal(ds[...], complex_array)
+    np.testing.assert_array_equal(ds.attrs["c"], c)
 
 
-def test_explicit_create(writable_file):
+@pytest.mark.parametrize("h5type_str, dt", H5_VS_NUMPY_DTYPES)
+def test_explicit_create(writable_file, h5type_str, dt):
     """Explicitly use native complex datatype to create datasets & attributes"""
-    for h5_dt, np_dt_s in [
-        (h5t.COMPLEX_IEEE_F32LE, "<c8"),
-        (h5t.COMPLEX_IEEE_F32BE, ">c8"),
-        (h5t.COMPLEX_IEEE_F64LE, "<c16"),
-        (h5t.COMPLEX_IEEE_F64BE, ">c16"),
-        (h5t.NATIVE_FLOAT_COMPLEX, "=c8"),
-        (h5t.NATIVE_DOUBLE_COMPLEX, "=c16"),
-    ]:
-        np_dt = np.dtype(np_dt_s)
-        complex_array = (np.random.rand(100) + 1j * np.random.rand(100)).astype(np_dt)
-        ds = writable_file.create_dataset(np_dt_s, (100,), dtype=h5py.Datatype(h5_dt))
-        ds[:] = complex_array
-        c = np.array(1.9 + 1j * 6.7, dtype=np_dt)
-        ds.attrs.create("c", c, dtype=h5py.Datatype(h5_dt))
-        assert isinstance(ds.id.get_type(), h5t.TypeComplexID)
-        assert isinstance(ds.attrs.get_id("c").get_type(), h5t.TypeComplexID)
-        np.testing.assert_array_equal(ds[...], complex_array)
-        np.testing.assert_array_equal(ds.attrs["c"], c)
+    h5type = getattr(h5t, h5type_str)
+    np_dt = np.dtype(dt)
+    complex_array = (np.random.rand(100) + 1j * np.random.rand(100)).astype(np_dt)
+    ds = writable_file.create_dataset(dt, (100,), dtype=h5py.Datatype(h5type))
+    ds[:] = complex_array
+    c = np.array(1.9 + 1j * 6.7, dtype=np_dt)
+    ds.attrs.create("c", c, dtype=h5py.Datatype(h5type))
+    assert isinstance(ds.id.get_type(), h5t.TypeComplexID)
+    assert isinstance(ds.attrs.get_id("c").get_type(), h5t.TypeComplexID)
+    np.testing.assert_array_equal(ds[...], complex_array)
+    np.testing.assert_array_equal(ds.attrs["c"], c)
 
 
 def test_dtype_cmpnd_cmplx():
     """Check the resulting dtype of the two-field compound data as complex numbers"""
     with h5py.File(get_data_file_path("compound-dtype-complex.h5"), mode="r") as f:
-        for obj in f.values():
-            if isinstance(obj, h5py.Dataset):
-                check_compound_complex_datatype(obj.id.get_type(), obj.dtype)
+        for obj in filter(lambda obj: isinstance(obj, h5py.Dataset), f.values()):
+            check_compound_complex_datatype(obj.id.get_type(), obj.dtype)
