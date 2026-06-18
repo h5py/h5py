13d927f5af2aeb198237c8db4f06f1aa3133f6c3
Clément Robert <cr52@protonmail.com>
2026-06-18 10:27:01 +0200

    CI: run abi3 builds on push if commit message contains `'[pip-pre]'`

--- ci/triage_build.sh
+++ ci/triage_build.sh
@@ -15,11 +15,13 @@ PYTHON=$3
 if [[ "$PYTHON" == "" ]]; then
     PYTHON="*"
 fi
-MSG="$(git show -s --format=%s $SHA)"
+TIP_COMMIT_MSG="$(git show -s --format=%s $SHA)"
+echo "TIP_COMMIT_MSG=$TIP_COMMIT_MSG" | tee -a $GITHUB_ENV
+
 KIND="$RUNNER_OS $ARCH"
 
 # If it's a scheduled build or [pip-pre] in commit message, use pip-pre
-if [[ "$GITHUB_EVENT_NAME" == "schedule" ]] || [[ "$MSG" = *'[pip-pre]'* ]]; then
+if [[ "$GITHUB_EVENT_NAME" == "schedule" ]] || [[ "$TIP_COMMIT_MSG" = *'[pip-pre]'* ]]; then
     echo "Using NumPy pip-pre wheel; setting CIBW_BEFORE_BUILD and CIBW_BUILD_FRONTEND"
     echo "CIBW_BEFORE_BUILD=pip install --pre --only-binary numpy --extra-index-url https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \"numpy>=2.0.0.dev0\" \"Cython>=0.29.31,<4\" packaging>=23.0 pkgconfig \"setuptools>=77\"" | tee -a $GITHUB_ENV
     echo "CIBW_BUILD_FRONTEND=pip; args: --no-build-isolation" | tee -a $GITHUB_ENV
