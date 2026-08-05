1effaa702afd8b836140b3886082c5d7a387c9cd
Clément Robert <cr52@protonmail.com>
2026-08-05 12:41:06 +0200

    debug

--- setup_configure.py
+++ setup_configure.py
@@ -133,7 +133,11 @@ class BuildConfig:
         ]) > 1:
             raise ValueError(
                 "Specify only one of: HDF5 lib/include dirs, HDF5 prefix dir, "
-                "or HDF5 pkgconfig name"
+                "or HDF5 pkgconfig name. Received:\n"
+                f"HDF5={hdf5}\n"
+                f"HDF5_INCLUDEDIR={hdf5_includedir}\n"
+                f"HDF5_LIBDIR={hdf5_libdir}\n"
+                f"HDF5_PKGCONFIG_NAME={hdf5_pkgconfig_name}\n"
             )
 
         if hdf5_includedir or hdf5_libdir:
